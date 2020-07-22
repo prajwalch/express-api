aa43cc01defb02ddbcb9f12dc1c3af3a34722aae
Prajwal Chapagain <prajjwal2058@gmail.com>
2020-07-22 09:56:22 +0545

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 const apiController = require('../controllers/api.controller');
 
-router.get('/v1/data', apiController.v1);
+router.get('/data', apiController.v1);
 
 module.exports = router;
